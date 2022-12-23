1116337460e463a2182d6782e75e2e1752ebae53
pradhan prasanta <pradhan.prasanta78@gmail.com>
2022-12-23 20:46:26 +0530

    Added the Basic Templete .Net 6 API.Data & Test Project

---
+++ Technology/Domain.API/Program.cs
@@ -0,0 +1,31 @@
+using Domain.Data.BusinessObjects;
+using Domain.Data.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddTransient<ISupplierBO, SupplierBO>();
+builder.Services.AddTransient<ISupplierCore, SupplierCore>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
