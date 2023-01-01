d584cf736cbfbbc014e2b3aa489e3553dd1e63d6
pradhan prasanta <pradhan.prasanta78@gmail.com>
2023-01-01 13:09:24 +0530

    Add The Unit Test

---
+++ Domain.Test/Controllers/ControllersUnitTest.cs
@@ -0,0 +1,35 @@
+using Domain.API.Controllers;
+using Domain.Data.Core;
+using Domain.Test.MockData;
+using Moq;
+
+namespace Domain.Test.Controllers
+{
+    public class ControllersUnitTest
+    {
+        private SupplierController _controller;
+        private Mock<ISupplierCore> _supplierCoreMock;
+
+        public ControllersUnitTest()
+        {
+            _supplierCoreMock = new Mock<ISupplierCore>();
+            _controller = new SupplierController(_supplierCoreMock.Object);
+
+        }
+
+        [Fact]
+        public void Get_Should_Return_All_Supplyer()
+        {
+            _supplierCoreMock.Setup(s => s.GetSupplierList()).Returns(new SupplierMock().AllSupplier());
+            var output = _controller.Get();
+            Assert.True(((Microsoft.AspNetCore.Mvc.ObjectResult)output.Result).StatusCode == 200);
+        }
+        [Fact]
+        public void GetById_Should_Return_All_Supplyer()
+        {
+            _supplierCoreMock.Setup(s => s.GetSupplierByIdAsync(1)).Returns(Task.FromResult(new SupplierMock().AllSupplier()[0]));
+            var output = _controller.Get(1);
+            Assert.NotNull(output);
+        }
+    }
+}
---
+++ Domain.Test/MockData/SupplierMock.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Data.SimpleModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Test.MockData
+{
+    public  class SupplierMock
+    {
+       private   List<SupplierSampleModel> Suppliers =new List<SupplierSampleModel>();
+        public SupplierMock()
+        {
+            Suppliers.Add(
+                new SupplierSampleModel 
+                {
+                    Address= "Address",
+                    City= "City",
+                    CompanyName= "CompanyName",
+                    ContactName= "ContactName",
+                    ContactTitle= "ContactTitle",
+                    Country= "Country",
+                    Fax= "Fax",
+                    HomePage= "HomePage",
+                    Phone= "Phone",
+                    PostalCode= "PostalCode",
+                    Region= "Region",
+                    SupplierID=1
+                }
+                );
+        }
+        public   List<SupplierSampleModel> AllSupplier()
+        {
+            return Suppliers;
+        }
+    }
+}
