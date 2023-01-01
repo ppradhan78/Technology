c974801ea63019a8f0553e691ece4abb7b60c85b
pradhan prasanta <pradhan.prasanta78@gmail.com>
2023-01-01 14:00:27 +0530

    Added Test Case

--- Domain.Test/Controllers/SupplierControllerUnitTest.cs
+++ Domain.Test/Controllers/SupplierControllerUnitTest.cs
@@ -5,12 +5,12 @@ using Moq;
 
 namespace Domain.Test.Controllers
 {
-    public class ControllersUnitTest
+    public class SupplierControllerUnitTest
     {
         private SupplierController _controller;
         private Mock<ISupplierCore> _supplierCoreMock;
 
-        public ControllersUnitTest()
+        public SupplierControllerUnitTest()
         {
             _supplierCoreMock = new Mock<ISupplierCore>();
             _controller = new SupplierController(_supplierCoreMock.Object);
@@ -22,7 +22,7 @@ namespace Domain.Test.Controllers
         {
             _supplierCoreMock.Setup(s => s.GetSupplierList()).Returns(new SupplierMock().AllSupplier());
             var output = _controller.Get();
-            Assert.True(((Microsoft.AspNetCore.Mvc.ObjectResult)output.Result).StatusCode == 200);
+            Assert.True(((Microsoft.AspNetCore.Mvc.ObjectResult)output?.Result)?.StatusCode == 200);
         }
         [Fact]
         public void GetById_Should_Return_All_Supplyer()
---
+++ Domain.Test/Data/Core/SupplierCoreUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Data.BusinessObjects;
+using Domain.Data.Core;
+using Domain.Test.MockData;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Test.Data.Core
+{
+    public  class SupplierCoreUnitTest
+    {
+        private SupplierCore _supplierCore;
+        private Mock<ISupplierBO> _supplierBOMock;
+
+        public SupplierCoreUnitTest()
+        {
+            _supplierBOMock = new Mock<ISupplierBO>();
+            _supplierCore = new SupplierCore(_supplierBOMock.Object);
+
+        }
+
+        [Fact]
+        public void Get_Should_Return_All_Supplyer()
+        {
+            _supplierBOMock.Setup(s => s.GetSupplierList()).Returns(new SupplierMock().AllSupplier());
+            var output = _supplierCore.GetSupplierList();
+            Assert.True(output .Count>0);
+        }
+        [Fact]
+        public void GetById_Should_Return_All_Supplyer()
+        {
+            _supplierBOMock.Setup(s => s.GetSupplierByIdAsync(1)).Returns(Task.FromResult(new SupplierMock().AllSupplier()[0]));
+            var output = _supplierCore.GetSupplierByIdAsync(1);
+            Assert.NotNull(output);
+        }
+    }
+}
